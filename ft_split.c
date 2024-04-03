5e41040bc72319390b4b0f061fae6b2fc7002202
Alvaro Morales De Vera <alvmoral@c2r18s5.42madrid.com>
2024-04-03 14:50:12 +0200

    paco test passed for split. protected from NULL strings

--- ft_split.c
+++ ft_split.c
@@ -22,31 +22,44 @@ static int	len_cal(char const *s, char c)
 	token = 0;
 	while (s[i])
 	{
-		if (s[i] == c && s[i + 1] != c && s[i + 1] != '\0')
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
 			token += 1;
 		i++;
 	}
-	if (s[0] != c)
-		token++;
 	return (token);
 }
 
 static void	get_sep(unsigned int *start, unsigned int *next,
 char const *s, char c)
 {
+	int				found;
+	unsigned int	i;
+
+	found = 0;
+	i = *next;
+
 	*next = 0;
 	while (s[*start] && s[*start] == c)
 	{
 		*start = *start + 1;
-		//printf("start: %u\t next: %u\n", *start, *next);
 	}
 	while (s[*start] && s[*start] != c)
 	{
 		*next = *next + 1;
 		*start = *start + 1;
-		//printf("start: %u\t next: %u\n", *start, *next);
 	}
 	//printf("----------\n");
+	// while (s[i] && !found)
+	// {
+	// 	if (s[i] == c && s[i + 1] != c)
+	// 		*start = i + 1;
+	// 	if (s[i] != c && (s[i + 1] == c || !s[i + 1]))
+	// 	{
+	// 		*next = i + 1;
+	// 		found = 1;
+	// 	}
+	// 	i++;
+	// }
 }
 
 static char	**release(char **marr, int i)
@@ -68,6 +81,8 @@ char	**ft_split(char const *s, char c)
 	unsigned int		next;
 	char				**marr;
 
+	if (s == NULL)
+		return (NULL);
 	len = len_cal(s, c);
 	i = 0;
 	start = 0;
@@ -79,7 +94,6 @@ char	**ft_split(char const *s, char c)
 	{
 		get_sep(&start, &next, s, c);
 		marr[i] = ft_substr(s, start - next, next);
-		//printf("marri: %s\n", marr[i]);
 		if (marr[i] == NULL)
 			return (release(marr, i));
 		i++;
@@ -90,8 +104,8 @@ char	**ft_split(char const *s, char c)
 
 // int	main(void)
 // {
-// 	char	*s = "     olol      ";
-// 	char	**marr = ft_split(s, ' ');
+// 	char	s[] = "";
+// 	char	**marr = ft_split(s, 'z');
 // 	int		i = 0;
 
 // 	while (marr[i] != NULL)
