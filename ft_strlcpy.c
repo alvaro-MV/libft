36019e6e2c388e08e88af40cd2548a9803ad7112
Alvaro Morales De Vera <alvmoral@c2r15s2.42madrid.com>
2024-04-04 18:54:30 +0200

    strlcpy fixedd

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: alvmoral <alvmoral@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 18:45:42 by alvmoral          #+#    #+#             */
-/*   Updated: 2024/03/22 13:07:02 by alvmoral         ###   ########.fr       */
+/*   Updated: 2024/04/04 18:30:40y alvmoral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	tlen;
-	int		i;
+	size_t	i;
 
 	i = 0;
 	tlen = ft_strlen(src);
-	if (dstsize > 0)
+	if (dstsize != 0)
 	{
-		while (i < (int) dstsize -1 && src[i])
+		while (i < dstsize -1 && src[i])
 		{
 			dst[i] = src[i];
 			i++;
@@ -34,17 +34,12 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 
 // int	main(void)
 // {
-// 	char		dst[] = "Estamos aquí probando";
-// 	char		dstp[] = "Estamos aquí probando";
-// 	char const	src[] = "Asi ";
+// 	char		dst[] = "AAAAAAAAAA";
+// 	char		dstp[] = "AAAAAAAAAA";
+// 	char const	src[] = "coucou";
 // 	int			dstsize = 8;
 
-// 	printf("blen: %zu \t dst: %s\n", strlcpy(dst, src, dstsize), dst);
-// 	printf("blen: %zu \t dst: %s", ft_strlcpy(dstp, src, dstsize), dstp);
+// 	// printf("blen: %zu \t dst: %s\n", strlcpy(dst, src, -1), dst);
+// 	printf("blen: %zu \t dst: %s\n", ft_strlcpy(dstp, src, -1), dstp);
+// 	printf("logic: %d", !strcmp(src, dstp) && dst[strlen(src) + 1] == 'A');
 // }
-
-/*Cuando se dstsize es mayor que la longitud de dstp, entoces strlcpy 
-genera un error hardware instruction,
-mientras que mi función no; no se hasta que punto tengo que replicar eso.
-
-*/
